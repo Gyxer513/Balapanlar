586608d0b94f5451a8adf7dba4f9d7403599e675
Gyxer513 <fpanchuk@yandex.ru>
2022-08-17 15:52:08 +0300

    animation added

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,19 @@
 import "../pages/index.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+
+gsap.registerPlugin(ScrollTrigger);
+let sections = gsap.utils.toArray(".panel");
+
+gsap.to(sections, {
+  xPercent: -145* (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".pinck-scroll",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=2000",
+  }
+});
