61996acf2f3c3813b2b223f633335e191d333050
Anastasiya Bazaeva <anastasiya.albertowna@yandex.ru>
2022-08-18 16:34:45 +0300

    changes after merge resolved

--- src/components/index.js
+++ src/components/index.js
@@ -77,21 +77,21 @@ pythonButton.addEventListener('click', (evt) => {
     openPopup(coursePopup);
     renderPopup(popupContainer, createPopup('.course-python'));
 })
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
-gsap.registerPlugin(ScrollTrigger);
-let sections = gsap.utils.toArray(".panel");
+// gsap.registerPlugin(ScrollTrigger);
+// let sections = gsap.utils.toArray(".panel");
 
-gsap.to(sections, {
-  xPercent: -145* (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".pinck-scroll",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: "+=2000",
-  }
-
+// gsap.to(sections, {
+//   xPercent: -145* (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".pinck-scroll",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     // base vertical scrolling on how wide the container is so it feels more natural.
+//     end: "+=2000",
+//   }
+// })
