81b3857acd5b4f53455c5e0d5b9eee2826bb738a
Gyxer513 <fpanchuk@yandex.ru>
2022-08-29 16:15:15 +0300

    scroll bug fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,66 +1,76 @@
 import "../pages/index.css";
-import { lecturersButton, horizonsButton, himbioButton, pythonButton,
-	lecturersPopup, horizonsPopup, himbioPopup, coursePopup, popupContainer,
-	openPopup, closePopup, createPopup, renderPopup, anyPopup } from "../components/modals.js";
+import {
+	lecturersButton,
+	horizonsButton,
+	himbioButton,
+	pythonButton,
+	lecturersPopup,
+	horizonsPopup,
+	himbioPopup,
+	coursePopup,
+	popupContainer,
+	openPopup,
+	closePopup,
+	createPopup,
+	renderPopup,
+	anyPopup,
+} from "../components/modals.js";
 
-const header = document.querySelector('.header');
-const menue = document.querySelector('.burger-menu');
-const headerLogo = document.querySelector('.header__logo');
-const options = document.querySelector('.options');
-const stickOne = document.querySelector('#bar1');
-const stickTwo = document.querySelector('#bar2');
+const header = document.querySelector(".header");
+const menue = document.querySelector(".burger-menu");
+const headerLogo = document.querySelector(".header__logo");
+const options = document.querySelector(".options");
+const stickOne = document.querySelector("#bar1");
+const stickTwo = document.querySelector("#bar2");
 
 // изменение хэдэра при скролле
 window.onscroll = () => {
 	const Y = window.scrollY;
 
 	if (Y > 27) {
-		header.classList.add('header_small');
-		headerLogo.classList.add('header__logo_scroll');
+		header.classList.add("header_small");
+		headerLogo.classList.add("header__logo_scroll");
 	} else if (Y < 27) {
-		headerLogo.classList.remove('header__logo_scroll');
-		header.classList.remove('header_small');
+		headerLogo.classList.remove("header__logo_scroll");
+		header.classList.remove("header_small");
 	}
-}
+};
 
 // разворачивание бургерного меню на мобильном разрешении
-options.addEventListener('click', () => {
-	menue.classList.toggle('burger-menu_opened');
-	stickOne.classList.toggle('options__stick_rotate-one');
-	stickTwo.classList.toggle('options__stick_rotate-two');
-
-})
+options.addEventListener("click", () => {
+	menue.classList.toggle("burger-menu_opened");
+	stickOne.classList.toggle("options__stick_rotate-one");
+	stickTwo.classList.toggle("options__stick_rotate-two");
+});
 
 // слушатели для открытия попапов партнеров
-lecturersButton.addEventListener('click', () => openPopup(lecturersPopup));
+lecturersButton.addEventListener("click", () => openPopup(lecturersPopup));
 
-horizonsButton.addEventListener('click', () => openPopup(horizonsPopup));
+horizonsButton.addEventListener("click", () => openPopup(horizonsPopup));
 
-himbioButton.addEventListener('click', () => openPopup(himbioPopup));
+himbioButton.addEventListener("click", () => openPopup(himbioPopup));
 
 // слушатель для закрытия попапов
 anyPopup.forEach((popup) => {
-	popup.addEventListener('mousedown', (evt) => {
-		if (evt.target.classList.contains('popup_opened')) {
-			closePopup(popup)
+	popup.addEventListener("mousedown", (evt) => {
+		if (evt.target.classList.contains("popup_opened")) {
+			closePopup(popup);
 		}
-		if (evt.target.classList.contains('popup__button')) {
-			closePopup(popup)
+		if (evt.target.classList.contains("popup__button")) {
+			closePopup(popup);
 		}
-	})
-})
+	});
+});
 
 // слушатель для открытия попапа курса Питон
-pythonButton.addEventListener('click', (evt) => {
+pythonButton.addEventListener("click", (evt) => {
 	evt.preventDefault();
 	openPopup(coursePopup);
-	renderPopup(popupContainer, createPopup('.course-python'));
-})
-
+	renderPopup(popupContainer, createPopup(".course-python"));
+});
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger.js";
-
 
 gsap.registerPlugin(ScrollTrigger);
 const sections = gsap.utils.toArray(".panel");
@@ -77,21 +87,21 @@ const sections = gsap.utils.toArray(".panel");
 			// base vertical scrolling on how wide the container is so it feels more natural.
 			end: "+=1500",
 		},
-	})
+	});
 
-	window.addEventListener('resize', (event) => {
+const trigger = ScrollTrigger.getById("scrollObject");
 
-		const trigger = ScrollTrigger.getById("scrollObject");
+if (document.body.clientWidth < 1439) {
+	trigger.disable();
+} else {
+	trigger.enable
+};
 
-		if (document.body.clientWidth < 1439) {
-			trigger.disable();
-		} else {
-			trigger.enable({reset: true});
-		}
-
-	})
-
+window.addEventListener("resize", (event) => {
 	
-
-	
-
+	if (document.body.clientWidth < 1439) {
+		trigger.disable();
+	} else {
+		trigger.enable({ reset: true });
+	}
+});
